c47ccafa1ea76bf8b0cd6d1b4f25ec2c69bddd83
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-18 17:55:47 +0100

    Wrote the main logic in pseudo code, turned everything into comments

---
+++ index.js
@@ -0,0 +1,32 @@
+// GAME FUNCTIONALITIES
+
+// Function to take the user input
+//   User choice variable
+//   Prompt the user for rock, paper or scissor (case insensitive!)
+//     Check if the user choice is viable (one of the three listed above)
+//     If yes set the choice to that
+//     Else exit the game (while loop or not?)
+//   Return the user choice 
+
+// Function to make the computer choose randomly
+//   Computer choice variable
+//   Get a random number between 1 and 3
+//   If number is 1 rock, if 2 paper, if 3 scissor
+//     Set the computerchoice variable accordingly
+//   return the choice
+
+// Function to play a round(takes computer and user choice as arguments)
+//   Check which wins (RPS logic)
+//   If player wins
+//     return victory message
+//   If computer wins
+//     return defeat message
+//   If it's a tie play again
+
+// Main game function to combine the above
+//   Computer score variable
+//   Player score varible
+//   Round count varible
+//   Play the game five times
+//   Compare scores to declare a winner
+//   Higher score wins!
